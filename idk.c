488c718254d4f529931c9cacecadadceebd77357
Mayur <rautmayur2517@gmail.com>
2022-05-06 21:00:43 +0530

    made incomplete changes

--- idk.c
+++ idk.c
@@ -7,15 +7,15 @@ char printMenu();
 void calculate();
 void canceled();
 // Global Variables
-int price, quantity, retry;
+int retry, i;
 
 // Global Structure.
 struct orders
 {
     char name[25];
     int price;
-    int quant;
-} order;
+    int quantity;
+} order1, order2, order3, order3, order4, order5;
 
 void main()
 {
@@ -51,9 +51,8 @@ char printMenu()
     case 1:
         printf("\nYou have selected Panner Masla.");
         printf("\nPlease Enter your Quantity\n--> ");
-        scanf("%d", &quantity);
-        order.quant = quantity;
-        order.price = 40;
+        scanf("%d", &order1.quantity);
+        order1.price = 40;
         printf("\nDo you want to add-on?(1-Yes, 2-No, 3-Cancel)\n--> ");
         scanf("%d", &r);
 
@@ -61,12 +60,13 @@ char printMenu()
         {
             system("cls"); // clear screen.
             {
+                ++i;
                 printMenu();
             }
         }
         else if (r == 2)
         {
-            strcpy(order.name, "Panner Masla");
+            strcpy(order1.name, "Panner Masla");
             calculate();
         }
 
@@ -82,10 +82,9 @@ char printMenu()
     case 2:
         printf("\nYou have selected Shahi Panner.");
         printf("\nPlease Enter your Quantity\n--> ");
-        scanf("%d", &quantity);
+        scanf("%d", &order2.quantity);
 
-        order.price = 45;
-        land = quantity * 8.00;
+        order2.price = 45;
 
         printf("\nDo you want to add-on?(1-Yes, 2-No, 3-Cancel)\n--> ");
         scanf("%d", &r);
@@ -99,6 +98,7 @@ char printMenu()
         }
         else if (r == 2)
         {
+            strcpy(order2.name, "Shahi Panner");
             calculate();
         }
 
@@ -114,10 +114,8 @@ char printMenu()
     case 3:
         printf("\nYou have selected Shevbahaji.");
         printf("\nPlease Enter your Quantity\n--> ");
-        scanf("%d", &quantity);
-
-        order.price = 50;
-        land = quantity * 8.00;
+        scanf("%d", &order3.quantity);
+        order3.price = 50;
 
         printf("\nDo you want to add-on?(1-Yes, 2-No, 3-Cancel)\n--> ");
         scanf("%d", &r);
@@ -131,6 +129,7 @@ char printMenu()
         }
         else if (r == 2)
         {
+            strcpy(order3.name, "Shevbhahaji");
             calculate();
         }
 
@@ -146,10 +145,8 @@ char printMenu()
     case 4:
         printf("\nYou have selected Chicken Biryani.");
         printf("\nPlease Enter your Quantity\n--> ");
-        scanf("%d", &quantity);
-
-        order.price = 70;
-        land = quantity * 8.00;
+        scanf("%d", &order4.quantity);
+        order4.price = 70;
 
         printf("\nDo you want to add-on?(1-Yes, 2-No, 3-Cancel)\n--> ");
         scanf("%d", &r);
@@ -163,6 +160,7 @@ char printMenu()
         }
         else if (r == 2)
         {
+            strcpy(order4.name, "Chicken Biryani");
             calculate();
         }
 
@@ -178,10 +176,8 @@ char printMenu()
     case 5:
         printf("\nYou have selected Chicken Kabab.");
         printf("\nPlease Enter your Quantity\n--> ");
-        scanf("%d", &quantity);
-
-        order.price = 80;
-        land = quantity * 8.00;
+        scanf("%d", &order5.quantity);
+        order5.price = 80;
 
         printf("\nDo you want to add-on?(1-Yes, 2-No, 3-Cancel)\n--> ");
         scanf("%d", &r);
@@ -190,11 +186,13 @@ char printMenu()
         {
             system("cls"); // clear screen.
             {
+                ++i;
                 printMenu();
             }
         }
         else if (r == 2)
         {
+            strcpy(order5.name, "Chicken Kabab");
             calculate();
         }
 
@@ -242,7 +240,7 @@ canceled:
 
 void calculate()
 {
-    int cgst, sgst, total;
+    int cgst, sgst, total, cal_order, cal_price, cal_quantity;
 
     printf("              1 two ka four Restaurant\n");
     printf("        G.T ROAD, MURTHAL, SONIPAT, HARYANA\n");
@@ -251,20 +249,31 @@ void calculate()
     printf("---------------------------------------------------\n");
     printf("Order Names                  Qty    Value    Price\n");
     printf("---------------------------------------------------\n");
+    
     // Place for order names
-    printf("%s                %d        0         %d\n", order.name, order.quant, order.price);
+    printf("%s                %d        0         %d\n", order1.name, order1.quantity, order1.price);
+    printf("%s                %d        0         %d\n", order2.name, order2.quantity, order2.price);
+    printf("%s                %d        0         %d\n", order3.name, order3.quantity, order3.price);
+    printf("%s                %d        0         %d\n", order4.name, order4.quantity, order4.price);
+    printf("%s                %d        0         %d\n", order5.name, order5.quantity, order5.price);
+
     printf("---------------------------------------------------\n");
 
-    order.price = order.price * quantity; // adding quantity 
-    printf("SUBTOTAL                                      %3d\n", order.price);
+    // adding all order price together.
+    cal_price = order1.price + order2.price + order3.price + order4.price + order5.price;
+    // adding all order quantity together.
+    cal_quantity = order1.quantity + order2.quantity + order3.quantity + order4.quantity + order5.quantity;
 
-    sgst = order.price * 9 / 100; // adding sgst.
+    cal_price = cal_price * cal_quantity; // adding quantity
+    printf("SUBTOTAL                                      %3d\n", cal_price);
+
+    sgst = cal_price * 9 / 100; // adding sgst.
     printf("Add S GST(9.000%%) on XX.X                     %3d\n", sgst);
-    
-    cgst = order.price * 9 / 100; // adding cgst.
+
+    cgst = cal_price * 9 / 100; // adding cgst.
     printf("Add C GST(9.000%%) on XX.X                     %3d\n", cgst);
 
-    total = sgst + cgst + order.price; // addition of taxes to generate total amount.
+    total = sgst + cgst + cal_price;                                    // addition of taxes to generate total amount.
     printf("Ammount Incl of All Taxes                     %3d\n", total); // final amount would be displayed.
 
     printf("\n");
